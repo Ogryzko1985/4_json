34f72e1c8f549a9ccbc3e5f60a3062a9a2e0725b
Константин Огрызко <user16@avikon.local>
2017-05-03 12:12:27 +0300

    Исправлены названия переменных

--- pprint_json.py
+++ pprint_json.py
@@ -1,20 +1,20 @@
 import json
 import os.path
-def find_path1():
-    dir=os.path.abspath(__file__)  
-    find_path1.file_dir = dir[:-14]+'text.txt'
-    print(find_path1.file_dir)
-    return find_path1.file_dir
+def find_path():
+    directory=os.path.abspath(__file__)  
+    find_path.file_directory = directory[:-14]+'text.txt'
+    print(find_path.file_directory)
+    return find_path.file_directory
 def load_data(filepath):
     data = open(filepath, 'r')
-    load_data.read_ = data.read()  
-    return load_data.read_
+    load_data.read_file = data.read()  
+    return load_data.read_file
 def pretty_print_json(data):
-    pretty_print_json.decode_ = json.loads(data)
-    print(json.dumps(pretty_print_json.decode_,sort_keys=True,indent = 4,ensure_ascii=False))
+    pretty_print_json.decode_file = json.loads(data)
+    print(json.dumps(pretty_print_json.decode_file,sort_keys=True,indent = 4,ensure_ascii=False))
     
 
 if __name__ == '__main__':
-    find_path1()
-    load_data(find_path1.file_dir)
-    pretty_print_json(load_data.read_)
+    find_path()
+    load_data(find_path.file_directory)
+    pretty_print_json(load_data.read_file)
